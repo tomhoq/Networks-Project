e62b7c46b96bbbd10c6217f2efb69e17373ac6a8
Tomaz <tomhoq2003@gmail.com>
2023-12-08 18:03:10 +0000

    get user auctions not finished

--- server-development/db.c
+++ server-development/db.c
@@ -106,6 +106,28 @@ int is_password_correct(const char * file_path, char *password){
     }
 }
 
+int is_auction_active(char *auction_id) {
+    char auction_directory[30];
+    char end_file[50];
+
+    sprintf(auction_directory, "./AUCTIONS/%s/", auction_id);
+    sprintf(end_file, "%sEND_%s.txt", auction_directory, auction_id);
+    printf("auction_directory: %s\n", auction_directory);
+    printf("end_file: %s\n", end_file);
+
+    if (!directory_exists(auction_directory)) {
+        printf("Directory does not exist\n");
+        return -1;
+    }
+    else if (!file_exists(end_file)) {
+        printf("Auction is active.\n");
+        return 1;
+    } else {
+        printf("Auction is not active.\n");
+        return 0;
+    }
+}
+
 //-Funcoes complexas-----------------------------------------------------------------------------------------------------------------------------------------
 
 int login_user(char username[10], char pass[10]) {
@@ -243,19 +265,160 @@ int logout(char username[10], char password[10]){
     }
 }
 
+int unregister(char username[10], char password[10]){
+
+    // Verification
+    if (strlen(username) != 6 || strlen(password) != 8) { 
+        printf("Username or password too long.\n");
+        return -1;
+    }
+
+    char user_directory[30];
+    char pass_file[50];
+    char login_file[50];
+
+    sprintf(user_directory, "./USERS/%s/", username);
+    sprintf(pass_file, "%s%s_pass.txt", user_directory, username);
+    sprintf(login_file, "%s%s_login.txt", user_directory, username);
+
+    if (!directory_exists(user_directory)) {
+        printf("User not registered.\n");
+        return UNR;
+    } 
+    if (!file_exists(pass_file)) {
+        printf("User not registered. No pass_file\n");
+        return UNR;
+    }
+    else if (!file_exists(login_file)) {
+        printf("User not logged in.\n");
+        return NOK;
+    }
+    else{
+        if (is_password_correct(pass_file, password)) {
+            printf("Password is correct.\n");
+            delete_file(login_file);
+            delete_file(pass_file);
+            return OK;
+        } else {
+            printf("Password is incorrect.\n");
+            return NOK;
+        }
+    }
+}
+
+char *get_user_auctions(char username[10]){
+    char user_directory[30];
+    char login_file[50];
+    char hosted_dir[50];
+    DIR *dir;
+    struct dirent *entry;
+
+    sprintf(user_directory, "./USERS/%s/", username);
+    sprintf(login_file, "%s%s_login.txt", user_directory, username);
+    sprintf(hosted_dir, "%sHOSTED/", user_directory);
+
+    if (!directory_exists(user_directory)) {
+        printf("User not registered.\n");
+        return NULL;
+    }
+    else if (!directory_exists(hosted_dir)) {
+        printf("This should not happen. ERROR AT DB\n");
+        return NULL;
+    }
+    else if (!file_exists(login_file)) {
+        printf("User not logged in.\n");
+        return NULL;
+    }
+    else{
+        int number_files = 0;
+        char *auctions;
+        DIR *dir;
+        struct dirent *ent;
+        if ((dir = opendir (hosted_dir)) != NULL) {
+            while ((ent = readdir (dir)) != NULL) {
+                if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
+                    number_files += 1;
+                }
+            }
+            if (closedir(dir) == -1) {
+                printf("Error closing directory\n");
+                return NULL;
+            }
+        }
+
+        if (number_files == 0) {
+            printf("No auctions hosted.\n");
+            return NULL;
+        }
+        auctions = malloc(number_files * 7 * sizeof(char));
+        memset(auctions, '\0', 100);
+        char auc[5];
+        memset(auc, '\0', 5);
+
+        if ((dir = opendir (hosted_dir)) != NULL) {
+            while ((ent = readdir (dir)) != NULL) {
+                number_files -= 1;
+                if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
+                    char *dotPosition = strchr(ent->d_name, '.');
+                    // Copy only the part before the period (if any)
+                    if (dotPosition != NULL) {
+                        strncat(auc, ent->d_name, dotPosition - ent->d_name);
+                        strcat(auctions, auc);
+                        if (is_auction_active(auc)) {
+                            strcat(auctions, " 1");
+                        } else {
+                            strcat(auctions, " 0");
+                        }
+                        printf("b: %s\n, %d\n", ent->d_name, dotPosition - ent->d_name);
+                    } else {
+                        return NULL;
+                    }
+                    if (number_files != 0)
+                        strcat(auctions, " ");
+                    else
+                        strcat(auctions, "\n");
+                    memset(auc, '\0', 5);
+                }
+            }
+            if (closedir(dir) == -1) {
+                printf("Error closing directory\n");
+                free(auctions);
+                return NULL;
+            }
+        } else {
+            free(auctions);
+            printf("Error opening directory\n");
+            return NULL;
+        }
+        return auctions;
+    }
+}
 
 int main() {
     char username[10] = "234234";
     char password[10] = "23423423";
 
     
-    if (login_user("234234", "23423423") == 0) {
-        printf("User registered successfully.\n");
-    } else {
-        printf("Failed to register user.\n");
+    if (unregister("234234", "23423423") == 0) { // corrigir!
+        printf("User login successfully.\n");
+    } 
+    /*if
+    char *s = get_user_auctions("234234");  // corrigir!
+    printf("Auctions: %s\n", s);
+    free(s);
+
+    s = get_user_auctions("123123");
+    printf("Auctions: %s\n", s);
+    if (s == NULL) {
+        printf("0 auctions.\n");
     }
+
+
+    //sleep(1);
+    
     printf("logging out\n");
     printf("%d\n", logout("234234", "23423423"));
+    printf("%d\n", unregister("234234", "23423423"));*/
 
     return 0;
 }
