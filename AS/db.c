e0dadc39736fed6b1eed5b5d0877110cf3b6e3d4
Tomaz Silva <tomhoq2003@gmail.com>
2023-12-13 20:23:33 +0000

    began close and implemented check availability

--- AS/db.c
+++ AS/db.c
@@ -17,6 +17,9 @@
 #define REG 12
 #define UNR 13
 #define NLG 14
+#define EAU 15
+#define EOW 16
+#define END 17
 
 #define last 666
 #define UNITIALIZED 667
@@ -99,6 +102,32 @@ int file_exists(const char *file_path) {
     return access(file_path, F_OK) != -1;
 }
 
+//returns a string with how many seconds have passed since UNIX EPOCH, 1970
+char* get_seconds_elapsed() {
+    time_t current_time;
+    time(&current_time);
+    char *time_string = malloc(12 * sizeof(char));
+    snprintf(time_string, 12, "%ld", (long)current_time);
+
+    return time_string;
+}
+
+char* get_date() {
+    time_t fulltime;
+    struct tm *current_time;
+    char *time_string = malloc(25 * sizeof(char));
+    memset(time_string, '\0', 25);
+
+    time(&fulltime);
+
+    current_time = gmtime(&fulltime);
+
+    sprintf(time_string,"%4d-%02d-%02d %02d:%02d:%02d", current_time->tm_year + 1900, current_time->tm_mon + 1, current_time->tm_mday, 
+                                                        current_time->tm_hour, current_time->tm_min, current_time->tm_sec);
+
+    return time_string;
+}
+
 int directory_exists(const char *path) {
     DIR *dir = opendir(path);
 
@@ -135,6 +164,17 @@ int create_directory(const char *dir_path) {
     return 0;  // Directory created successfully
 }
 
+int folder_exists(const char *dir_path) {
+    DIR *dir = opendir(dir_path);
+
+    if (dir != NULL) {
+        closedir(dir);
+        return 1;  // Directory exists
+    } else {
+        return 0;  // Directory doesn't exist or an error occurred
+    }
+}
+
 int is_password_correct(const char * file_path, char *password){
     char *p = malloc(10 * sizeof(char));
     memset(p, 0, 10);
@@ -153,53 +193,111 @@ int is_password_correct(const char * file_path, char *password){
     }
 }
 
+//devolve 1 se tiver ativo e 0 senao
+int check_validity(char *auction_id){
+    char start_file[50];
+    char auction_directory[30];
+    char end_file[50];
+
+    sprintf(auction_directory, "./AUCTIONS/%s/", auction_id);
+    sprintf(start_file, "%sSTART_%s.txt", auction_directory, auction_id);   
+
+    if (!directory_exists(auction_directory)) {
+        //printf("Directory does not exist\n");
+        return -1;
+    } else if (!file_exists(start_file)) {
+        printf("Start file does not exist\n");
+        return -1;
+    } else {
+        char buffer[140];
+        memset(buffer, '\0', sizeof(buffer));
+        if (read_file(start_file, buffer, sizeof(buffer)-1) == -1) {
+            printf("Error reading start file");
+            return -1;
+        }
+        if (strlen(buffer) == 0){
+            printf("ERROR AT DB. Empty Start file.\n");
+            return -1;
+        }
+        char *start_fulltime = malloc(15 * sizeof(char));
+        memset(start_fulltime, '\0', 15);
+        char *time_active = malloc(15 * sizeof(char));
+        memset(time_active, '\0', 15);
+        if (sscanf(buffer, "%*s %*s %*s %*s %s %*s %*s %s", time_active, start_fulltime) != 2){
+            printf("ERROR AT DB-> Wrong content in start file->\n");
+            return -1;
+        }
+
+        // convert to int and compare with current time
+        int time_active_int = atoi(time_active);
+        int start_fulltime_int = atoi(start_fulltime);
+        int current_time_int = atoi(get_seconds_elapsed());
+
+        free(start_fulltime);
+        free(time_active);
+
+        //printf("time_active_int: %d\n", time_active_int);
+        //printf("start_fulltime_int: %d\n", start_fulltime_int);
+        printf("old?: %d\n", current_time_int - (start_fulltime_int + time_active_int));
+
+        if (current_time_int > (start_fulltime_int + time_active_int)) {
+            printf("Auction is not active.\n");
+            sprintf(end_file, "%sEND_%s.txt", auction_directory, auction_id);
+            memset(buffer, '\0', sizeof(buffer));
+            char *date = get_date();
+            char *secs = get_seconds_elapsed();
+            sprintf(buffer, "%s %s", date, secs);
+            free(date);
+            free(secs);
+
+            printf("buffer: %s\n", buffer);
+
+            if (create_file(end_file, buffer) == -1) {
+                printf("Error creating end file");
+                return -1;
+            }
+            return 0; // auction not active
+        } else {
+            printf("Auction is active.\n");
+            return 1;
+        }
+
+        return 1;
+    }
+}
+
+//devolve 1 se activo, 0 se inativo
 int is_auction_active(char *auction_id) {
     char auction_directory[30];
     char end_file[50];
 
     sprintf(auction_directory, "./AUCTIONS/%s/", auction_id);
     sprintf(end_file, "%sEND_%s.txt", auction_directory, auction_id);
-    printf("auction_directory: %s\n", auction_directory);
-    printf("end_file: %s\n", end_file);
+    //printf("auction_directory: %s\n", auction_directory);
+    //printf("end_file: %s\n", end_file);
 
     if (!directory_exists(auction_directory)) {
         printf("Directory does not exist\n");
         return -1;
     }
     else if (!file_exists(end_file)) {
-        printf("Auction is active.\n");
-        return 1;
+        //verificar se tempo ja passou
+        int n;
+        if ((n = check_validity(auction_id)) == 1) {
+            printf("Auction is active.\n");
+            return 1;
+        } else if (n == 0)  {
+            printf("Auction is not active.\n");
+            return 0;
+        } else {
+            printf("Error checking validity\n");
+        }
     } else {
         printf("Auction is not active.\n");
         return 0;
     }
 }
 
-//returns 
-char* get_seconds_elapsed() {
-    time_t current_time;
-    time(&current_time);
-    char *time_string = malloc(12 * sizeof(char));
-    snprintf(time_string, 12, "%ld", (long)current_time);
-
-    return time_string;
-}
-
-char* get_date() {
-    time_t fulltime;
-    struct tm *current_time;
-    char *time_string = malloc(25 * sizeof(char));
-    memset(time_string, '\0', 25);
-
-    time(&fulltime);
-
-    current_time = gmtime(&fulltime);
-
-    sprintf(time_string,"%4d-%02d-%02d %02d:%02d:%02d", current_time->tm_year + 1900, current_time->tm_mon + 1, current_time->tm_mday, 
-                                                        current_time->tm_hour, current_time->tm_min, current_time->tm_sec);
-
-    return time_string;
-}
 
 //-Funcoes complexas-----------------------------------------------------------------------------------------------------------------------------------------
 
@@ -393,7 +491,7 @@ int get_user_auctions(char username[10], auction list[1000]){
 
     if (!directory_exists(user_directory)) {
         printf("User not registered.\n");
-        return NOK;
+        return NLG;
     }
     else if (!directory_exists(hosted_dir)) {
         printf("This should not happen. ERROR AT DB.\n");
@@ -462,7 +560,7 @@ int get_user_bids(char username[10], auction list[1000]){
 
     if (!directory_exists(user_directory)) {
         printf("User not registered.\n");
-        return NOK;
+        return NLG;
     }
     else if (!directory_exists(bidded_dir)) {
         printf("This should not happen. ERROR AT DB\n");
@@ -716,7 +814,7 @@ int create_auction(char uid[8], char password[10], char auction_name[20], char s
     //check login------------------------------------------------
     if (!directory_exists(user_directory)) {
         printf("User not registered.\n");
-        return NOK;
+        return NLG;
     }
     else if (!directory_exists(hosted_dir)) {
         printf("This should not happen. ERROR AT DB.\n");
@@ -796,10 +894,77 @@ int create_auction(char uid[8], char password[10], char auction_name[20], char s
         delete_all(a_dir);
         return NOK;
     }
-    //sprintf(a_dir, "%s%s/", auctions_dir, aid);
+    char auction_path[70];
+    sprintf(auction_path, "%s%s.txt", hosted_dir, aid);
 
+    if (create_file(auction_path, "") == -1){
+        delete_all(a_dir);
+        return NOK;
+    }   
+    return OK;
 
 }
+
+int close_auction(char uid[10], char password[10], char aid[5]){
+    char user_directory[30];
+    char login_file[50];
+    char pass_file[50];
+    char hosted_dir[50];
+    char hosted_auction[70];
+    char a_dir[25];
+
+    sprintf(user_directory, "./USERS/%s/", uid);
+    sprintf(login_file, "%s%s_login.txt", user_directory, uid);
+    sprintf(pass_file, "%s%s_pass.txt", user_directory, uid);
+    sprintf(hosted_dir, "%sHOSTED/", user_directory);
+    sprintf(hosted_auction, "%s%s.txt", hosted_dir, aid);
+    sprintf(a_dir, "./AUCTIONS/%s/", aid);
+
+    //check login------------------------------------------------
+    if (!directory_exists(user_directory)) {
+        printf("User not registered.\n");
+        return NLG;
+    }
+    else if (!file_exists(login_file)) {
+        printf("User not logged in.\n");
+        return NLG;
+    }
+    else if (!file_exists(pass_file)) {
+        printf("User is not registered.\n");
+        return NLG;
+    }
+    else if(is_password_correct(pass_file, password) == 0) {
+        return NOK;
+    }
+    if (!folder_exists(a_dir)) {
+        printf("Auction does not exist.\n");
+        return EAU;
+    } else if (!file_exists(hosted_auction)) {
+        printf("%s\n", a_dir);
+        return EOW;
+    } 
+    if (!is_auction_active(aid)) { //closes auction if duration has finished
+        return END;
+    } else {
+        char buffer[140] , end_file[50];
+        memset(buffer, '\0', sizeof(buffer));
+        char *date = get_date();
+        char *secs = get_seconds_elapsed();
+        sprintf(buffer, "%s %s", date, secs);
+        sprintf(end_file, "%sEND_%s.txt", a_dir, aid);
+        free(date);
+        free(secs);
+
+        printf("buffer: %s\n", buffer);
+
+        if (create_file(end_file, buffer) == -1) {
+            printf("Error creating end file");
+            return NOK;
+        }
+        return OK;
+    }
+}
+
 
 int main() {
     char username[10] = "234234";
@@ -809,7 +974,8 @@ int main() {
     //login_user("345345", "34534534");
 
     //delete_all("./USERS/345345/");
-    printf("create: %d\n", create_auction("234234", "23423423", "auchan", "100", "3600", "file.txt"));
+    //printf("create: %d\n", create_auction("234234", "23423423", "auchan", "100", "100", "file.txt"));
+    printf("%d\n", close_auction(username, password, "004"));
 
     /*
     auction a;
